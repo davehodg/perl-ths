95417a77788a3059c38124bf17b712820107a877
David Hodgkinson <david.hodgkinson@openimp.com>
2023-08-24 14:41:47 +0200

    fiddled a bit

--- pagegen.pl
+++ pagegen.pl
@@ -13,29 +13,17 @@ use Config::Any;
 use Data::Dumper;
 use Carp;
 
-<<<<<<< HEAD
 `rm html/*`;
-<<<<<<< HEAD
-=======
-
-my $tt = Template->new({
-    INCLUDE_PATH => './tt',
-#    PRE_PROCESS  => 'header',
-#    POST_PROCESS => 'footer',
-    #INTERPOLATE  => 1,
-}) || die "$Template::ERROR\n";
->>>>>>> 6a83380 (Update to new Trond format)
-=======
-my $ini_file = q{./config.ini};
+ 
+ my $ini_file = q{./config.ini};
 my @files = ($ini_file);
 my $cfg = Config::Any->load_files({files => \@files, use_ext => 1});
 #warn Dumper($cfg);
 my $url = $cfg->[0]{$ini_file}{spreadsheet}{url};
-#warn $url;
+warn $url;
 
-`mkdir html ; rm html/*`;
-`curl -L -o deep.csv $url`;
->>>>>>> 5490cc8 (get csv properly)
+`rm -rf html ; mkdir html`;
+`curl -Ls -o deep.csv $url`;
 
 my $tt = Template->new(
     {
@@ -47,37 +35,20 @@ my $csv = Text::CSV_XS->new( { binary => 1, auto_diag => 1 } );
 open my $fh, "<:encoding(utf8)", "deep.csv" or croak "deep.csv: $!";
 
 my $t = <$fh>;
-my $t = <$fh>;
 my $lineup;
 my $fout;
 
-<<<<<<< HEAD
 my $headers = $csv->getline($fh);
 while ( my $row = $csv->getline($fh) ) {
     $lineup = $row->[0];
     my $fname = 'html/' . $lineup . '.html';
-<<<<<<< HEAD
     open $fout, '>>', $fname
       || croak "Can't open $fname";
-=======
-my $headers = $csv->getline($fh);   
-while (my $row = $csv->getline ($fh)) {
-    # $row->[0] contains the file name
-    $lineup = $row->[0];
-    my $fname = 'html/' . $lineup . '.html';
-    open $fout, '>>', $fname 
-         || croak "Can't open $fname";
-
-    
->>>>>>> 6a83380 (Update to new Trond format)
-=======
->>>>>>> 5490cc8 (get csv properly)
 
     {
         my $url = $row->[5];
         my $image;
 
-<<<<<<< HEAD
         if ( $url =~ /youtube/x ) {
             my @junk = split( /v=/, $url );
             if ( defined $junk[1] ) {
@@ -115,32 +86,8 @@ while (my $row = $csv->getline ($fh)) {
             || croak "Can't open $fname";
         say $fout $out;
         close($fout);
-=======
-        if ($url =~ /youtube/) {
-            my @junk = split (/v=/, $url);
-            if (defined $junk[1]) {
-                $image = "https://i.ytimg.com/vi/" . $junk[1] . "/hqdefault.jpg";
-            }
-            #warn $image;
-        }
-        
-        my $vars = {
-            title => $row->[2],
-            url   => $row->[5],
-            image => $image,
-        };
-        my $out;
-        $tt->process('row.tt', $vars, \$out) || croak $tt->error(), "\n";
-
-        say $fout $out;
-        close ($fout);
->>>>>>> 6a83380 (Update to new Trond format)
     }
 }
 
 close $fh;
-<<<<<<< HEAD
-=======
-
->>>>>>> 6a83380 (Update to new Trond format)
 
