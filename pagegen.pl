9bc91ac806c00a2aa91ad87907267c4fc8d9b7a5
David Hodgkinson <david.hodgkinson@openimp.com>
2023-10-26 11:37:50 +0200

    fix ->

--- pagegen.pl
+++ pagegen.pl
@@ -8,19 +8,21 @@ use open qw( :std :encoding(UTF-8) );
 use Text::CSV_XS qw(csv);
 use Template;
 use Config::Any;
-
+use HTML::Escape qw/escape_html/;
 
 use Data::Dumper;
 use Carp;
 
 `rm html/*`;
- 
- my $ini_file = q{./config.ini};
-my @files = ($ini_file);
-my $cfg = Config::Any->load_files({files => \@files, use_ext => 1});
+
+my $ini_file = q{./config.ini};
+my @files    = ($ini_file);
+my $cfg      = Config::Any->load_files( { files => \@files, use_ext => 1 } );
+
 #warn Dumper($cfg);
 my $url = $cfg->[0]{$ini_file}{spreadsheet}{url};
-warn $url;
+
+#warn $url;
 
 `rm -rf html ; mkdir html`;
 `curl -Ls -o deep.csv $url`;
@@ -34,11 +36,14 @@ my $tt = Template->new(
 my $csv = Text::CSV_XS->new( { binary => 1, auto_diag => 1 } );
 open my $fh, "<:encoding(utf8)", "deep.csv" or croak "deep.csv: $!";
 
-my $t = <$fh>;
+#my $t = <$fh>;
 my $lineup;
 my $fout;
 
 my $headers = $csv->getline($fh);
+
+#warn Dumper($headers);
+
 while ( my $row = $csv->getline($fh) ) {
     $lineup = $row->[0];
     my $fname = 'html/' . $lineup . '.html';
@@ -72,18 +77,22 @@ while ( my $row = $csv->getline($fh) ) {
             #exit ;
         }
 
+        $row->[2] =~ s/\-\>//g;
+
+        #$image =~ s/Plzeň/Boing!/;
+        warn $row->[2];
         my $vars = {
-            title    => $row->[2],
+            title    => escape_html( $row->[2] ),
             date     => $row->[3],
             duration => $row->[4],
             url      => $row->[5],
-            image    => $image,
+            image    => escape_html($image),
         };
         my $out;
         $tt->process( 'row.tt', $vars, \$out ) || croak $tt->error(), "\n";
 
         open $fout, '>>', $fname
-            || croak "Can't open $fname";
+          || croak "Can't open $fname";
         say $fout $out;
         close($fout);
     }
